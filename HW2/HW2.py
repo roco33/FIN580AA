629045a38380d1afa5e24b6433aa7b60032489d8
roco33 <minhaogu@gmail.com>
2017-11-27 20:08:25 -0600

    HW2

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -11,17 +11,26 @@ import numpy as np
 
 
 def importData():
+    
     # asset names set
     asset = ['Russell 1000', 'Russell 2000', 'BofA Merrill Lynch US Corp Master', 'BofA Merrill Lynch US High Yield', '3-Month Treasury Bill', 'Consumer Price Index for All Urban Consumers', 'EAFE Standard (Large+Mid Cap)', 'EM (EMERGING MARKETS) Standard (Large+Mid Cap)']
     # file name
     file = 'Data for HW 2 Fall 2017.csv'
     # skip rows
     skip = [i for i in range(9)]
-#    skip.append(10)
     # read csv into dataframe
-    rawData = pd.read_csv(file,skiprows = skip,usecols = [i for i in range(1,9,1)])
-    rawData.columns = asset
-    return rawData
+    rawData = pd.read_csv(file,skiprows = skip,usecols = [i for i in range(9)])
+    rawData.columns = ['year'] + asset
+    rawData = rawData.set_index('year')
+    #return
+    returnData = np.log(rawData/rawData.shift(1))
+    returnData['3-Month Treasury Bill'] = rawData['3-Month Treasury Bill'] / 100
+    returnData = returnData[1:]
+    
+    return returnData
 
 
 data = importData()
+cov = data.cov()
+sd = data.std()
+
